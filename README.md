0dca0204a43e0f2dc95bcbc50d915b6d3e2f40d5
Yuki XD <pxnditxyr@gmail.com>
2021-09-18 23:44:41 -0400

    Initializing React Application Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# TODO APP in Typescript
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(
+    <h1> Hola Mi Meloshitaaaaa Amada </h1>,
+    document.querySelector( "#root" ),
+);
